c0afc725907df9af679a1be57bfd827d8c45dca1
eliz-bzh <43619988+eliz-bzh@users.noreply.github.com>
2022-01-09 03:42:39 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./styles.css";
+
+export default function App() {
+  const hidden = (id, e) => {
+    let card = document.getElementsByClassName("card");
+    card[id].className += " cardHidden";
+  };
+  return (
+    <div className="App d-flex justify-content-center row">
+      {[1, 2, 3].map((item, index) => (
+        <div className="row" key={index}>
+          <div className="col">
+            <div className="card" style={{ width: "18rem" }} index={index}>
+              <div className="card-body">
+                <h5 className="card-title">Card title {index}</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <button
+                  className="btn btn-danger"
+                  onClick={(e) => hidden(index, e)}
+                >
+                  X
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
